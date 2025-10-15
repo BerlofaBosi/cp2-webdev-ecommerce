e49fae696a82669046b846b5a746398e54b61732
Arthur Berlofa Bosi <berlofabosi.arthur@gmail.com>
2025-10-14 23:01:55 -0300

    Ultima mudança estetica

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,7 @@
 export default function Section({titulo, children}) {
     return (
         <div>
-            <h2 className="w-full flex justify-center bg-black text-white">{titulo}</h2>
+            <h2 className="text-2xl h-auto p-2 w-full flex justify-center bg-black text-white">{titulo}</h2>
             <div className="grid grid-cols-4 gap-6 p-5">
                 {children}
             </div>
